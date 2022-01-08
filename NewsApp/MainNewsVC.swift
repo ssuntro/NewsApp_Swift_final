723d542d2a7344e827325b7dbc2fd248c9398bf7
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-08 21:24:48 +0700

    [AC-3]  MainNewsVC - conforms TableDatasource and tableDelegate

--- NewsApp/MainNewsVC.swift
+++ NewsApp/MainNewsVC.swift
@@ -7,26 +7,37 @@
 
 import UIKit
 
-class MainNewsVC: UIViewController {
+let highLightNews = [NewsSection(count: 3, color: UIColor.lightGray),
+                   NewsSection(count: 2, color: UIColor.darkGray)]
 
+
+class MainNewsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension MainNewsVC  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        highLightNews[section].count
     }
     
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "goToDemoVC" {
-            if let demoVC = segue.destination as? DemoVC {
-                demoVC.name = "Booooo"
-            }
-        }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        highLightNews.count
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as UITableViewCell
+        cell.backgroundColor = highLightNews[indexPath.section].color
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
