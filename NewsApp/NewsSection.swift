534bd1a8d7e308759dab91b9be2279c8640bce44
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-08 21:22:52 +0700

    [AC-3] new struct NewsSection

---
+++ NewsApp/NewsSection.swift
@@ -0,0 +1,14 @@
+//
+//  NewsSection.swift
+//  NewsApp
+//
+//  Created by Siroratt Suntronsuk on 8/1/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+struct NewsSection {
+    let count: Int
+    let color: UIColor
+}
