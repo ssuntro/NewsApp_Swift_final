11451dac20d5adf540f74adc6ff4aa6ceab7fed1
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-08 17:53:42 +0700

    [AC-1] animate dinosaur

---
+++ NewsApp/LaunchVC.swift
@@ -0,0 +1,45 @@
+//
+//  LaunchVC.swift
+//  NewsApp
+//
+//  Created by Siroratt Suntronsuk on 8/1/2565 BE.
+//
+
+import UIKit
+import Lottie
+
+class LaunchVC: UIViewController {
+    lazy var dinosaurView: AnimationView = {
+        let animationView = AnimationView()
+        let path = Bundle.main.path(forResource: "dino",
+                                        ofType: "json") ?? ""
+        animationView.animation = Animation.filepath(path)
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 0.5
+        return animationView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animateDinosaue()
+        // 5 sec
+        //stop animate
+    }
+
+    func animateDinosaue() {
+        view.addSubview(dinosaurView)
+        view.bringSubviewToFront(dinosaurView)
+        dinosaurView.play()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
