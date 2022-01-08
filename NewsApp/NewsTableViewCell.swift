e420e5fa349b3510d7b6743bcdeabc36a3274083
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-08 21:53:24 +0700

    [AC-3]  create NewsTableViewCell

---
+++ NewsApp/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  NewsApp
+//
+//  Created by Siroratt Suntronsuk on 8/1/2565 BE.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    @IBOutlet var typeBallon: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
