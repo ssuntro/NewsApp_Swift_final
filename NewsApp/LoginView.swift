903142f7072e22596f1f9f9faf823c2006b71ae7
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-20 00:19:37 +0700

    [AC-10] layout loginView

--- NewsApp/LoginView.swift
+++ NewsApp/LoginView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LoginView: View {
     @State var isLoginMode = false
     @State var email = ""
+    @State var password = ""
     var body: some View {
         NavigationView {
             ScrollView {
@@ -22,12 +23,34 @@ struct LoginView: View {
                             .tag(false)
                     }.pickerStyle(SegmentedPickerStyle())
                     
+                    if !isLoginMode {
+                        Button {
+                            imageButtonDidClick()
+                        } label: {
+                            Image(systemName: "person.fill")
+                        }
+
+                    }
                     TextField("Email", text: $email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
+                    SecureField("Password", text: $password)
+                    
+                    Button {
+                        buttonDidClick()
+                    } label: {
+                        Text(isLoginMode ? "Log In" : "Sign up")
+                    }
                 }
             }.navigationTitle(isLoginMode ? "Login": "Signup")
         }
+    }
+    
+    func buttonDidClick() {
+        
+    }
+    
+    func imageButtonDidClick() {
     }
 }
 
