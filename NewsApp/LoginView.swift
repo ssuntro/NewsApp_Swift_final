6f89150a80b57b000637b23f51d5be8a4e3f314c
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2022-01-21 22:20:30 +0700

    [AC-10,11] layout loginView + complete printOut functionality

--- NewsApp/LoginView.swift
+++ NewsApp/LoginView.swift
@@ -7,55 +7,90 @@
 
 import SwiftUI
 
+
 struct LoginView: View {
     @State var isLoginMode = false
+//    var isLoginMode = false
     @State var email = ""
     @State var password = ""
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack {
-                    Picker(selection: $isLoginMode,
-                           label: Text("Picker")) {
-                        Text("Login")
-                            .tag(true)
-                        Text("Signup")
-                            .tag(false)
-                    }.pickerStyle(SegmentedPickerStyle())
-                    
+                VStack(spacing: 16) {
+                    pickerView
                     if !isLoginMode {
-                        Button {
-                            imageButtonDidClick()
-                        } label: {
-                            Image(systemName: "person.fill")
-                        }
-
+                        imagePicker
                     }
-                    TextField("Email", text: $email)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                    SecureField("Password", text: $password)
                     
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    
+
                     Button {
                         buttonDidClick()
                     } label: {
+                        Spacer()
                         Text(isLoginMode ? "Log In" : "Sign up")
-                    }
-                }
-            }.navigationTitle(isLoginMode ? "Login": "Signup")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 10)
+                            .font(.system(size: 14, weight: .semibold))
+                        Spacer()
+                            
+                    }.background(.blue)
+                    
+                }.padding()
+            }
+            .navigationTitle(isLoginMode ? "Login": "Signup")
+            .background(Color.init(white: 0, opacity: 0.05))
+        }
+    }
+    
+    var pickerView: some View {
+        Picker(selection: $isLoginMode,
+               label: Text("Picker")) {
+            Text("Login")
+                .tag(true)
+            Text("Signup")
+                .tag(false)
+        }.pickerStyle(SegmentedPickerStyle())
+    }
+    
+    var imagePicker: some View {
+        Button {
+            imageButtonDidClick()
+        } label: {
+            VStack {
+                Image(systemName: "person.fill")
+                    .font(.system(size: 64))
+                    .padding()
+                    .foregroundColor(Color(.label))
+            }
+            .overlay(
+                    RoundedRectangle(cornerRadius: 64).stroke(Color.black, lineWidth: 3))
         }
     }
     
     func buttonDidClick() {
-        
+        print("signup/login button did click: \(email), \(password)")
     }
     
     func imageButtonDidClick() {
+        //IBAction did not called on preview action.
+        print("imageButtonDidClick")
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
+//        ZStack {LoginView()}
         LoginView()
+        
     }
 }
+
