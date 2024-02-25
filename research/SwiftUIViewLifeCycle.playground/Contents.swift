10e846bb44176d2235a870749618b6fa6e44874f
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-02-26 01:58:38 +0700

    [research] demo SwiftUI view lifecycle

---
+++ research/SwiftUIViewLifeCycle.playground/Contents.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+
+//https://www.hackingwithswift.com/quick-start/swiftui/how-to-respond-to-view-lifecycle-events-onappear-and-ondisappear
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                NavigationLink {
+                    DetailView()
+                } label: {
+                    Text("Hello World")
+                }
+            }
+            .onAppear {
+                print("ContentView appeared!")
+            }
+            .onDisappear {
+                print("ContentView disappeared!")
+            }
+        }
+    }
+}
+
+
+class DetailViewModel: ObservableObject {
+    deinit { //ava only class
+        print("DetailView is deinited")
+    }
+    init() {
+        print("DetailView is inited")
+    }
+}
+
+
+struct DetailView: View {
+    @StateObject private var vm = DetailViewModel()
+    var body: some View {
+        VStack {
+            Text("Second View")
+        }
+        .onAppear {
+            print("DetailView appeared!")
+        }
+        .onDisappear {
+            print("DetailView disappeared!")
+        }
+    }
+    
+
+}
+
+import PlaygroundSupport
+let viewController = UIHostingController(rootView: ContentView())
+PlaygroundPage.current.liveView = viewController
