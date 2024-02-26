de009757b4e63a8a9f21693160f30986653a7b2b
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-02-26 23:53:09 +0700

    [research.screen transition] create demo for multiple UI elem apply with matchedGeometryEffect

--- research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
+++ research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
@@ -7,16 +7,27 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                Text(verbatim: "SwiftUI")
-                    .font(.largeTitle.weight(.bold))
-                    .matchedGeometryEffect(id: "title", in: namespace)
+                VStack {
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    Text(verbatim: "SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                }
             }
             else {
-                Text(verbatim: "SwiftUI...")
-                    .font(.largeTitle.weight(.bold))
-                    .matchedGeometryEffect(id: "title", in: namespace)
+                VStack {
+                    Text(verbatim: "SwiftUI...")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .trailing)
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                }
             }
         }.onTapGesture {
             withAnimation {
