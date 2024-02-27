6b83b49dc85763754673c180379127d3ef6f8e74
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-02-27 23:54:24 +0700

    [research.screen transition] create matchedGeometryEffect on multiple UI styling elem such as bgColor

--- research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
+++ research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
@@ -14,26 +14,34 @@ struct MatchedView: View {
                     Text(verbatim: "SwiftUI")
                         .font(.largeTitle.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                .foregroundColor(.white)
+//                .background(.red)
+                .background(Color.red.matchedGeometryEffect(id: "background", in: namespace))
             }
             else {
                 VStack {
+                    Spacer()
                     Text(verbatim: "SwiftUI...")
                         .font(.largeTitle.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
                     Text("20 sections - 3 hours".uppercased())
                         .font(.footnote.weight(.semibold))
                         .matchedGeometryEffect(id: "subtitle", in: namespace)
                 }
+                .foregroundColor(.black)
+//                .background(.blue)
+                .background(Color.blue.matchedGeometryEffect(id: "background", in: namespace))
             }
         }.onTapGesture {
             withAnimation {
                 show.toggle()
             }
         }
-        
     }
 }
+//rule of thump
+//1 matchedGeo should be use on individual ele styling.
+//2. fit you add geometry in wrong position or none of geometry the transition will fall back to default one which is fade in and out
