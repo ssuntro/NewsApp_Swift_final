e55f9f9a15040be88e8507c84806c633ca408640
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-02-28 23:56:37 +0700

    [research.thread and queue] add examples to explore behaviour of Swift thread and queue

--- research/ThreadAndQueue.playground/Contents.swift
+++ research/ThreadAndQueue.playground/Contents.swift
@@ -83,3 +83,80 @@ var result: Result<Int, NetworkError> { get async {
 //}
 
 
+
+
+
+
+
+//example 1
+for i in 0..<20 {
+    print("Hi")
+    DispatchQueue.main.async {
+        print("index: \(i)")
+    }
+    print("done")
+    print("----")
+}
+
+
+////example 2
+//for i in 0..<10 {
+//    Task {
+//        print("thread: \(Thread.current)")
+//        print("is main thread: \(Thread.isMainThread)")
+//        try! await Task.sleep(nanoseconds: 3_000_000_000)
+//        print("index: \(i)")
+//    }
+//}
+
+
+////example 3
+//for i in 0..<10 {
+//    Task {
+//        print("thread: \(Thread.current)")
+//        print("is main thread: \(Thread.isMainThread)")
+//        try! await Task.sleep(nanoseconds: 3_000_000_000)
+//        print("index: \(i)")
+//    }
+//    print("Hi")
+//}
+
+
+
+////example 4
+//print("main thread: \(Thread.current)")
+//for i in 0..<10 {
+//    print("Hi")
+//    DispatchQueue.main.async {
+//        print("thread: \(Thread.current)")
+//        print("index: \(i)")
+//    }
+//    print("done")
+//    print("---")
+//}
+
+
+
+//example 5
+//print("main thread: \(Thread.current)")
+//for i in 0..<100 {
+//    print("Hi")
+//    DispatchQueue.global(qos: .background).async {
+//        print("thread: \(Thread.current)")
+//        print("index: \(i)")
+//    }
+//    DispatchQueue.main.async {
+//        print("thread: \(Thread.current)")
+//        print("index: \(i)")
+//    }
+//    print("done")
+//}
+
+////example 6
+//for i in 0..<100 {
+//    DispatchQueue.global(qos: .background).async {
+//        print("thread: \(Thread.current)")
+//        Thread.sleep(forTimeInterval: 3.0)
+//        print("index: \(i)")
+//    }
+//}
