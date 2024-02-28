62e0d2f8d6dfa39d97e28a803b0521d52270654d
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-02-28 15:41:50 +0700

    [research.custom transition between screens] add foreground and bg image with transition and rounded corner

--- research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
+++ research/CustomTrasitionBetweenScreens.swiftpm/ContentView.swift
@@ -15,14 +15,35 @@ struct MatchedView: View {
                         .font(.largeTitle.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
                         .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("Build an x app for iOS 18 with custom layouts, animations and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "text", in: namespace)
                 }
+                .padding(20)
                 .foregroundColor(.white)
-//                .background(.red)
-                .background(Color.red.matchedGeometryEffect(id: "background", in: namespace))
+//                .background(Color.red.matchedGeometryEffect(id: "background", in: namespace))
+                .background(
+                    Image(uiImage: UIImage(imageLiteralResourceName:  "8623701.png"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: "image", in: namespace))
+                
+                .background(
+                    Image(uiImage: UIImage(imageLiteralResourceName:  "Background 5.jpg"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace))
+                .mask(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace))
+                .padding(20)
             }
             else {
-                VStack {
+                VStack(alignment: .leading, spacing: 12) {
                     Spacer()
+                    Text("Build an iOS app for iOS 18 with custom layouts, animations and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "text", in: namespace)
                     Text(verbatim: "SwiftUI...")
                         .font(.largeTitle.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
@@ -33,7 +54,20 @@ struct MatchedView: View {
                 }
                 .foregroundColor(.black)
 //                .background(.blue)
-                .background(Color.blue.matchedGeometryEffect(id: "background", in: namespace))
+//                .background(Color.blue.matchedGeometryEffect(id: "background", in: namespace))
+                .background(
+                    Image(uiImage: UIImage(imageLiteralResourceName:  "8623701.png"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: "image", in: namespace))
+                .background(
+                    Image(uiImage: UIImage(imageLiteralResourceName:  "Background 5.jpg"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace))
+                .mask(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace))
             }
         }.onTapGesture {
             withAnimation {
@@ -44,4 +78,4 @@ struct MatchedView: View {
 }
 //rule of thump
 //1 matchedGeo should be use on individual ele styling.
-//2. fit you add geometry in wrong position or none of geometry the transition will fall back to default one which is fade in and out
+//2. fit you add geometry in wrong position or none of geometry the transition will fall back to default one which is fade in and out transition
