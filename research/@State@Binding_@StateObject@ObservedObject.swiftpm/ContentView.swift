1b96eeeb6059c6fce93cb250551612ffd3aecb76
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-03-10 23:27:30 +0700

    [research.@State@StateObject@ObservedObject] add one example each

--- research/@State@Binding_@StateObject@ObservedObject.swiftpm/ContentView.swift
+++ research/@State@Binding_@StateObject@ObservedObject.swiftpm/ContentView.swift
@@ -1,12 +1,35 @@
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var name: String = "eiei"
+}
+
 struct ContentView: View {
+    @StateObject var user = User()
+
+    @State var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Your name is \(user.name)")
+            TextField("Enter your name", text: $user.name)
+            TextField("Enter your counter", text: $message)
+            InnerView(user: user)
+                .background(Color.green)
         }
     }
+}
+import SwiftUI
+struct InnerView: View {
+    @ObservedObject var user: User
+
+    var body: some View {
+        Text("Hello, \(user.name)")
+            .onReceive(user.$name) { newName in
+                print("User name changed to \(newName)")
+            }
+        TextField("Inner view", text: $user.name)
+    }
+}
+#Preview {
+    ContentView(user: User())
 }
