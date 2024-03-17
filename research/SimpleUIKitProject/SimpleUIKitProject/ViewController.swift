f170457117b8f0d655248f86bb11540996ff6978
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-03-18 00:26:45 +0700

    [research] add example of  app and view lifecycle of[research] add example of  app and view lifecycle of UIKit project

---
+++ research/SimpleUIKitProject/SimpleUIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleUIKitProject
+//
+//  Created by Siroratt Suntronsuk on 17/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
